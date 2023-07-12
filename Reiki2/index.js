4c9089b95a2e2a119b9903493457b0e0df246e82
Churchred <98105070+Churchred@users.noreply.github.com>
2023-07-12 14:22:30 +0200

    chg

--- Reiki2/index.js
+++ Reiki2/index.js
@@ -1,5 +1,15 @@
 function click_btn(){
   const nav_bar = document.getElementsByClassName('dropdown')[0]
   nav_bar.classList.toggle('active')
-  console.log("CLICKED")
+  console.log("Dropdown: Active")
+}
+
+
+function unclick_btn(){
+  const nav_bar = document.getElementsByClassName('dropdown')[0]
+  
+  if(nav_bar.classList[1] == 'active'){
+    nav_bar.classList.toggle('active')
+    console.log("Dropdown: No longer active")
+  }
 }
